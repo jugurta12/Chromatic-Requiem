77976f39b9c638c5a9fd81ba97861bc173d00cc5
Jugurta12 <jugurta.abdessmed@icloud.com>
2025-11-27 18:39:10 +0100

    final healthbarre

--- Scripts/health.gd
+++ Scripts/health.gd
@@ -1,10 +1,14 @@
 extends AnimatedSprite2D
 
-@export var player : CharacterBody2D 
+@export var shake_intensity := 5
+@export var shake_duration := 0.2
+
+var original_position
+var previous_health := 100
+var shaking := false
 
 func _ready():
-	if player:
-		update_health_bar(player.health)
+	original_position = position
 
 func update_health_bar(new_health):
 	match new_health:
@@ -18,6 +22,22 @@ func update_health_bar(new_health):
 			play("25%")
 		0:
 			play("0%")
+	
+	if new_health < previous_health:
+		shake()
+	previous_health = new_health
 
 func _on_character_body_2d_health_changed(new_health):
 	update_health_bar(new_health)
+
+func shake():
+	if shaking:
+		return
+	shaking = true
+	var time := 0.0
+	while time < shake_duration:
+		position = original_position + Vector2(randf_range(-shake_intensity, shake_intensity), randf_range(-shake_intensity, shake_intensity))
+		time += get_process_delta_time()
+		await get_tree().process_frame
+	position = original_position
+	shaking = false
