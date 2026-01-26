e20b27146e55574dfe9a43c35cb899e43fe00397
Jugurta12 <jugurta.abdessmed@icloud.com>
2026-01-26 12:43:54 +0100

    add balise design

--- Scripts/balide.gd
+++ Scripts/balide.gd
@@ -1,9 +1,17 @@
-extends AnimatedSprite2D
+extends CharacterBody2D
+@onready var anim = $AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	anim.play("begin")
+	await get_tree().create_timer(0.60).timeout
+	anim.play("default")
+	while true:
+		await get_tree().create_timer(3).timeout
+		anim.play("atk")
+		await get_tree().create_timer(2.832).timeout
+		anim.play("default")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
