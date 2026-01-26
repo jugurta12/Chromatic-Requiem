b7345db6f7833cd39d71b4e01a5b00976a85214d
Jugurta12 <jugurta.abdessmed@icloud.com>
2026-01-26 20:37:21 +0100

    add balise atk script

--- Scripts/balide.gd
+++ Scripts/balide.gd
@@ -1,17 +1,26 @@
 extends CharacterBody2D
-@onready var anim = $AnimatedSprite2D
-
+@onready var anim = $CollisionShape2D/AnimatedSprite2D
+@onready var anime = $CollisionShape2D2/atk
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	anim.play("begin")
-	await get_tree().create_timer(0.60).timeout
+	anime.visible = false  
+	await get_tree().create_timer(0.59).timeout
 	anim.play("default")
 	while true:
 		await get_tree().create_timer(3).timeout
 		anim.play("atk")
-		await get_tree().create_timer(2.832).timeout
+		await get_tree().create_timer(2.5).timeout
+		anime.visible = true
+		anime.play("target")
+		await get_tree().create_timer(0.332).timeout
 		anim.play("default")
+		await get_tree().create_timer(3).timeout
+		anime.play("atk")
+		await get_tree().create_timer(0.49).timeout
+		anime.visible = false  
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
