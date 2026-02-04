b7be9b38d21cee609894396f2d6e37e79f2bbc90
Jugurta12 <jugurta.abdessmed@icloud.com>
2026-02-04 20:00:09 +0100

    add menu redesign and script

--- Scripts/pause.gd
+++ Scripts/pause.gd
@@ -2,17 +2,21 @@ extends CanvasLayer
 var menu = null
 
 func _ready():
-	menu = $TextureRect
-	remove_child(menu)  
+	menu = $AnimatedSprite2D
+	remove_child(menu) 
+	$Control.visible = false
+	 
 
 func _on_pause_pressed() -> void:
 	if menu.get_parent() == null:
 		add_child(menu) 
 		get_tree().paused = true
+		$Control.visible = true
 		
 	else:
 		remove_child(menu) 
 		get_tree().paused = false
+		$Control.visible = false
 
 func _on_button_pressed() -> void:
 	get_tree().paused = false
@@ -25,3 +29,16 @@ func _on_character_body_2d_game_over() -> void:
 	visible = false
 	if menu.get_parent() != null:
 		remove_child(menu)
+
+
+func _on_quit_pressed() -> void:
+	get_tree().paused = false
+	remove_child(menu)
+	$Control.visible = false  
+
+
+func _on_reset_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+	remove_child(menu) 
+	
